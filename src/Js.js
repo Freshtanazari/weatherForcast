48032cad53963b0440419baa080960441569dab5
Freshtanazari <freshtanazari0m@gmail.com>
2025-11-07 22:21:23 +0500

    fixed the bug in forecast section, refactored for readability and cleaned unnecessary UI elements

--- src/Js.js
+++ src/Js.js
@@ -1,7 +1,18 @@
-function searchAPI(city) {
-  let apikey = "fboa45182d680fb13e9a481dbtf60b57";
-  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
-  axios.get(url).then(updateWeather);
+const apikey = "fboa45182d680fb13e9a481dbtf60b57";
+const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+function searchAPI(city) { 
+  let currentURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(currentURL).then(updateWeather).catch(handleError);
+
+  let forecastURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}`;
+  axios.get(forecastURL).then(updateForecast).catch(handleError);
+}
+
+function handleError(error){
+  console.error("Weather API: ", error);
+  alert("City not found or network issue. Please Try Again")
 }
 
 function updateDate(datetime) {
@@ -16,20 +27,12 @@ function updateDate(datetime) {
     hour = `0${hour}`;
   }
 
-  Weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Satruday",
-  ];
   day = Weekdays[day];
   return `${day} ${hour}:${minutes}`;
 }
 
 function updateWeather(response) {
+
   console.log(response.data);
 
   let weatherCondition = document.querySelector("#weatherCondition");
@@ -53,47 +56,29 @@ function updateWeather(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
 
-  currentDateTime = new Date(response.data.time * 1000);
+  let currentDateTime = new Date(response.data.time * 1000);
   let currDate = document.querySelector("#currDate");
   currDate.innerHTML = updateDate(currentDateTime);
 
   let todayIcon = document.querySelector("#todayIcon");
   todayIcon.src = `${response.data.condition.icon_url}`;
-
-  getForcast(response.data.city);
 }
 
 function searchCity(event) {
   event.preventDefault();
+  const searchInput = document.querySelector("#searchInput");
   searchAPI(searchInput.value);
 }
 
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", searchCity);
 
-function getForcast(city) {
-  apikey = "fboa45182d680fb13e9a481dbtf60b57";
-  apiurl = `https://api.shecodes.io/weather/v1/forecast?query={query}&key=${apikey}`;
-  console.log(apiurl);
-  axios.get(apiurl).then(updateForcast);
-}
-
-function updateForcast(response) {
-  let forcastdays = "";
-
-  let Weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Satruday",
-  ];
+function updateForecast(response) {
+  let forecastdays = "";
 
   response.data.daily.forEach(function (day) {
-    forcastdays =
-      forcastdays +
+    forecastdays =
+      forecastdays +
       `
         <div class="forcastday">
             <p>${Weekdays[new Date(day.time * 1000).getDay()]}</p>
@@ -104,8 +89,8 @@ function updateForcast(response) {
         </div>`;
   });
 
-  let forcast = document.querySelector("#sevenDayWrapper");
-  forcast.innerHTML = forcastdays;
+  let forecast = document.querySelector("#sevenDayWrapper");
+  forecast.innerHTML = forecastdays;
 }
 
 searchAPI("Paris");
